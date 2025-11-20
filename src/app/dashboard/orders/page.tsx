ffdf8f9353d79522956e447c83b9307fe65bed65
akmal-adnan <81947525+akmal-adnan@users.noreply.github.com>
2025-11-20 23:08:01 +0800

    feat: order page header

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -1,9 +1,27 @@
 import styles from '@/app/dashboard/orders/styles.module.scss';
+import { Calendar, DocumentUpload } from 'iconsax-reactjs';
 
 const OrdersPage = () => {
   return (
     <div className={styles.mainContainer}>
-      <h6>Order page</h6>
+      <div className={styles.headerGroup}>
+        <div className={styles.titleTextContainer}>
+          <h6>Orders List</h6>
+          <p>Monitor and manage all customer orders in one place.</p>
+        </div>
+
+        <div className={styles.buttonGroup}>
+          <button className={styles.calendarButton}>
+            <Calendar size={24} />
+            August 2024
+          </button>
+
+          <button className={styles.calendarButton}>
+            <DocumentUpload size={24} />
+            Export
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/dashboard/orders/styles.module.scss.d.ts
+++ src/app/dashboard/orders/styles.module.scss.d.ts
@@ -1,1 +1,5 @@
+export declare const buttonGroup: string;
+export declare const calendarButton: string;
+export declare const headerGroup: string;
 export declare const mainContainer: string;
+export declare const titleTextContainer: string;
